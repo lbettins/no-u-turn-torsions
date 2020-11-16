fixed wrong gradient sign

tnuts/geometry.py
@@ -20,6 +20,7 @@ class Geometry:
         self.internal0 = copy.deepcopy(samp_obj.torsion_internal)
         self.geom0 = copy.deepcopy(self.internal0.cart_coords)
 
+        self.prev_internal = copy.deepcopy(self.internal0)
         self.internal = internal
         self.geom = self.internal.cart_coords
 
@@ -49,6 +50,7 @@ class Geometry:
             if internal.prim_coords[ind] > 0:
                 self.qk[ind] *= -1
                 self.signs[i] *= -1
+        print("Qks are",self.qk)
         self.symmetry_numbers = np.atleast_1d(syms)
         self.L = 2*np.pi/self.symmetry_numbers
         self.center_mod = lambda x: \
@@ -104,15 +106,20 @@ class Geometry:
 
     def check_dihedral(self, target):
         dihedrals = np.array([self.internal.calc_dihedral(self.internal.c3d, self.scans[i]-1) for i in range(self.n_rotors)])
+        
         net = self.center_mod(dihedrals - self.dihedrals0)
         self.xcur = net
-        err = self.center_mod(net - target)
+        err = -self.signs*np.around(net - target, 5)
         return net, err
 
-    def transform_geometry_to(self, x, incr_step=np.pi/12, err_thresh=1e-5, cart_rms_thresh=1E-9):
+    def transform_geometry_to(self, x,
+            incr_step=np.pi/12, err_thresh=1e-5, cart_rms_thresh=1E-9,
+            prev_fail=False):
         step = np.zeros(len(self.qk))
         tic = time.perf_counter()
         cur_dihedral, error = self.check_dihedral(x)
+        MAXCOUNT = 3*(np.pi // incr_step)
+        count = 0
         while (np.abs(error) > err_thresh).any():
             for err,ind in zip(error,self.torsion_inds):
                 step[ind] = self.qk[ind]*min(np.abs(err), np.abs(incr_step))*np.sign(err)
@@ -120,42 +127,19 @@ class Geometry:
                 self.internal.transform_int_step(step, cart_rms_thresh=cart_rms_thresh)
             except:
                 self.hard_reset()
-                return self.transform_geometry_to(x)
+                if prev_fail:
+                    return getXYZ(self.samp_obj.symbols, self.prev_internal.cart_coords)
+                return self.transform_geometry_to(x,prev_fail=True)
             cur_dihedral, error = self.check_dihedral(x)
+            count += 1
+            if count > MAXCOUNT:    # not ideal
+                print("ERROR in convergence; max n iterations reached")
+                print("Current position:",self.xcur)
+                print(getXYZ(self.samp_obj.symbols, self.prev_internal.cart_coords))
+                return getXYZ(self.samp_obj.symbols, self.prev_internal.cart_coords)
         toc = time.perf_counter()
-        print("CONVERGED", 'Time:', toc-tic, "seconds")
-        return getXYZ(self.samp_obj.symbols, self.internal.cart_coords)
-
-    def get_geometry_at(self, x):
-        """
-        Δx = x - xi
-        """
-        x = np.atleast_1d(x)
-        delta_x = x - self.xcur
-        #if (delta_x > np.pi/36).any():
-        #    x %= 2*np.pi/self.symmetry_numbers
-        step = np.zeros(len(self.qk))
-        for i,ind in enumerate(self.torsion_inds):
-            step[ind] = self.qk[ind] * delta_x[i]
-
-        def transform_geom(dq, internal, max_step=np.pi/36):
-            """
-            Internal coordinate transformation needs to be incremental
-            """
-            step_wide = np.array([-1 if dq_i > max_step else 1 if dq_i < -max_step\
-                else 0 for dq_i in dq])
-            if step_wide.any():
-                ddq = max_step*step_wide
-                #print((dq+ddq)[-self.n_rotors:])
-                evolve_dihedral_by(ddq, internal, cart_rms_thresh=1e-15)
-                return transform_geom(dq+ddq, internal)
-            else:
-                return evolve_dihedral_by(dq, internal)
-
-        xyz = transform_geom(step, self.internal, np.pi/36)
-        #if xyz is not self.geom:
-        #    self.geom = xyz
-        self.xcur = x
+        print("CONVERGED", 'Time:', toc-tic, "seconds with", count, "iterations.")
+        self.prev_internal = copy.deepcopy(self.internal)
         return getXYZ(self.samp_obj.symbols, self.internal.cart_coords)
 
     def get_energy_grad_at(self, x, which="energy"):
@@ -213,10 +197,17 @@ class Geometry:
             Bt_inv = np.linalg.pinv(B.dot(B.T)).dot(B)
     
             grad = Bt_inv.dot(grad)[self.torsion_inds]
-            grad *= self.signs  # In Hartree per rad
+            #signs = np.ones(self.n_rotors)
+            #for i,ind in enumerate(self.torsion_inds):
+            #    if self.internal.prim_coords[ind] > 0:
+            #        signs[i] *= -1
+            #if not (self.signs == signs).all():
+            #    print(self.signs,signs)
+            #grad *= signs
 
+            #grad *= self.signs  # In Hartree per rad
+            #grad *= -1
             #E += ((x-roundx)*grad).sum()
-
             log_trajectory(self.traj_log,x,E,grad)
             subprocess.Popen(['rm {input_path}/{file_name}.q.out'.format(input_path=self.path,
                 file_name=file_name)], shell=True)

tnuts/main.py
@@ -29,15 +29,15 @@ def NUTS_run(samp_obj,T,
     logpE = lambda E: -E    # E must be dimensionless
     logp, Z, modes = generate_umvt_logprior(samp_obj, T)
     syms = np.array([mode.get_symmetry_number() for mode in modes])
+    np.random.seed(42)
     Is = np.array([mode.get_I() for mode in modes])
-    print(Is)
-    #Is *= (constants.amu*(10**-20))
+    Ks = np.array([beta*mode.get_spline_fn()(0,2) for mode in modes])
+    variances = get_initial_mass_matrix(modes, T)
     geom = Geometry(samp_obj, samp_obj.torsion_internal, syms)
-    energy_fn = Energy(geom)
+    energy_fn = Energy(geom, beta)
     n_d = len(modes)
-    resolution = 4.20  #degrees
+    resolution = 5.0  #degrees
     step_scale = resolution*(np.pi/180) / (1/n_d)**(0.25)
-    print("Step scale is",step_scale)
     if not hpc:
         with pm.Model() as model:
             #x = pm.DensityDist('x', logp, shape=n_d, testval=np.random.uniform(-1,1,n_d)*np.pi)
@@ -47,23 +47,34 @@ def NUTS_run(samp_obj,T,
             alpha = pm.Deterministic('a', np.exp(-DeltaE))
             E_obs = pm.DensityDist('E_obs', lambda E: logpE(E), observed={'E':DeltaE})
         with model:
-            #step = pm.NUTS(target_accept=0.50, step_scale=0.15, adapt_step_size=False)
-            step = pm.NUTS(target_accept=0.7, step_scale=step_scale, early_max_treedepth=6,
+            step = pm.NUTS(target_accept=0.9, step_scale=step_scale, early_max_treedepth=6,
                     max_treedepth=7, adapt_step_size=False)
-            #step = pm.NUTS(target_accept=0.6, early_max_treedepth=5, max_treedepth=7)
+            step = pm.NUTS(scaling=1/Ks, is_cov=True,
+                    step_scale=step_scale, early_max_treedepth=6,
+                    max_treedepth=6, adapt_step_size=True)
+            step = pm.NUTS(target_accept=0.65, scaling=variances, is_cov=True,
+                    step_scale=step_scale, early_max_treedepth=6,
+                    max_treedepth=6, adapt_step_size=False)
             trace = pm.sample(nsamples, tune=tune, step=step, 
                     chains=nchains, cores=ncpus, discard_tuned_samples=False)
     else:
         with pm.Model() as model:
-            x = pm.DensityDist('x', logp, shape=n_d, testval=np.random.rand(n_d)*2*np.pi)
-            DeltaE = (beta*energy_fn(x))-\
-                    (-logp(x))
+            #x = pm.DensityDist('x', logp, shape=n_d, testval=np.random.rand(n_d)*2*np.pi)
+            x = pm.DensityDist('x', logp, shape=n_d, testval=np.zeros(n_d))
+            bE = pm.Deterministic('bE', energy_fn(x))
+            DeltaE = (bE)-(-logp(x))
             alpha = pm.Deterministic('a', np.exp(-DeltaE))
             E_obs = pm.DensityDist('E_obs', lambda E: logpE(E), observed={'E':DeltaE})
         with model:
-            #step = pm.NUTS(target_accept=0.5)
-            step = pm.NUTS(target_accept=0.7, step_scale=step_scale, early_max_treedepth=6,
-                    max_treedepth=6, adapt_step_size=False)
+            #step = pm.NUTS(target_accept=0.7, scaling=1/Ks, is_cov=True,
+            #        step_scale=step_scale, early_max_treedepth=6,
+            #        max_treedepth=6, adapt_step_size=False)
+            step = pm.NUTS(target_accept=0.35, scaling=variances, is_cov=True,
+                    step_scale=step_scale, early_max_treedepth=4,
+                    max_treedepth=5, adapt_step_size=True)
+            step = pm.NUTS(target_accept=0.65, scaling=variances, is_cov=True,
+                    step_scale=step_scale, early_max_treedepth=4,
+                    max_treedepth=5, adapt_step_size=True)
             trace = pm.sample(nsamples, tune=tune, step=step, 
                     chains=nchains, cores=1, discard_tuned_samples=False)
     Q = Z*np.mean(trace.a)
@@ -79,11 +90,31 @@ def NUTS_run(samp_obj,T,
         pkl_kwargs['n'] = n
     pickle.dump(model_dict,
             open(os.path.join(samp_obj.output_directory,pkl_file.format(**pkl_kwargs)),'wb'))
-    if not hpc:
+    if True:
         plot_MC_torsion_result(trace,modes,T)
         print("Prior partition function:\t", Z)
         print("Posterior partition function:\t", np.mean(trace.a)*Z)
         print("Expected likelihood:\t", np.mean(trace.a))
+        print("Best step size:", trace.get_sampler_stats("step_size_bar")[:tune+1])
+        print("Step size:", trace.get_sampler_stats("step_size")[:tune+1])
+
+def get_initial_mass_matrix(modes, T):
+    from scipy.integrate import quad
+    import rmgpy.constants as constants
+    fns = np.array([mode.get_spline_fn() for mode in modes])
+    sym_nums = np.array([mode.get_symmetry_number() for mode in modes])
+    beta = 1/(constants.kB*T)*constants.E_h
+    Ks = np.array([beta*mode.get_spline_fn()(0,2) for mode in modes])
+    variances = []
+    Zs = []
+    for fn,s in zip(fns,sym_nums):
+        Z = quad(lambda x: np.exp(-beta*fn(x)), -np.pi/s, np.pi/s)[0]
+        var = np.power(Z,-1)*quad(lambda y: np.power(y,2)*np.exp(-beta*fn(y)), -np.pi/s, np.pi/s)[0] -\
+                np.power(1/Z*quad(lambda y: y*np.exp(-beta*fn(y)), -np.pi/s, np.pi/s)[0],2)
+        Zs.append(Z)
+        variances.append(var)
+    print(variances, 1/Ks)
+    return np.array(variances)
 
 def generate_umvt_logprior(samp_obj, T):
     from tnuts.ops import LogPrior
@@ -96,8 +127,9 @@ def generate_umvt_logprior(samp_obj, T):
     energy_array = [mode.get_spline_fn() for mode in modes]
     # Get the symmetry numbers for each 1D torsion
     sym_nums = np.array([mode.get_symmetry_number() for mode in modes])
-    Z = np.array([quad(lambda x: fn(x), -np.pi/s, np.pi/s)[0]\
-            for fn,s in zip(energy_array,sym_nums)]).sum()
+    beta = 1/(constants.kB*T)*constants.E_h
+    Z = np.array([quad(lambda x: np.exp(-beta*fn(x)), -np.pi/s, np.pi/s)[0]\
+            for fn,s in zip(energy_array,sym_nums)]).prod()
 
     # Return theano op LogPrior and partition function
     return LogPrior(energy_array, T, sym_nums), Z, modes
@@ -128,7 +160,6 @@ def plot_MC_torsion_result(trace, NModes, T=300):
                                 for sym,xi in zip(syms,x)]) for x in xsamples[0]])
         #print(xsamples)
         samples=np.vstack(xsamples)
-        #samples=np.vstack(trace['xmod'])
         figure = corner.corner(samples, 
                 labels=["$x_{{{0}}}$".format(i) for i in range(1, n_d+1)],
                 hist_kwargs=hist_kwargs)

tnuts/ops.py
@@ -1,10 +1,12 @@
 import warnings
+import os
 import numpy as np
 import theano
 import theano.tensor as tt
 import pymc3 as pm
 import rmgpy.constants as constants
 import copy
+from tnuts.common import log_trajectory
 
 class LogPrior(tt.Op):
     itypes=[tt.dvector]
@@ -16,23 +18,28 @@ class LogPrior(tt.Op):
         self.ndim = len(fn_array)
         self.dlogp = LogPriorGrad(self.Epriors, self.beta, self.ndim)
         self.L = 2*np.pi/self.sym_ns
+        self.count = 0
         self.center_mod = lambda x: \
                 ((x.transpose() % self.L) \
                 - self.L*((x.transpose() % self.L) // ((self.L)/2))).transpose()
+        output_dir = os.path.expandvars("$SCRATCH")
+        traj_log = os.path.join(output_dir, 'nuts_out', 'logprior')
+        self.traj_log = traj_log
 
     def perform(self, node, inputs, outputs):
         theta, = inputs
-        #theta %= 2*np.pi/self.sym_ns
         theta = self.center_mod(theta)
         result = 0
+        grads = np.zeros(self.ndim)
         for i in range(self.ndim):
-            # Shift the domain over by pi
             result -= self.beta*self.Epriors[i](theta[i])
+            grads[i] = -self.beta*self.Epriors[i](theta[i], 1)
+        self.count += 1
+        #log_trajectory(self.traj_log, theta, result, grads)
         outputs[0][0] = np.array(result)
 
     def grad(self, inputs, g):
         theta, = np.array(inputs)
-        #theta %= 2*np.pi/self.sym_ns
         theta = self.center_mod(theta)
         return [g[0] * self.dlogp(theta)]
 
@@ -49,16 +56,16 @@ class LogPriorGrad(tt.Op):
         grads = np.zeros(self.ndim)
         for i in range(self.ndim):
             grads[i] = -self.beta*self.fns[i](theta[i], 1)
-        #print("LogPrior grad:",theta, -1.0/self.beta*grads)
         outputs[0][0] = grads
 
 class Energy(tt.Op):
     itypes = [tt.dvector] # expects a vector of parameter values when called
     otypes = [tt.dscalar] # outputs a single scalar value (the log likelihood)
-    def __init__(self, geom):
+    def __init__(self, geom, beta):
         self.geom = geom
+        self.beta = beta
         self.get_e_elect = self.geom.get_energy_grad_at
-        self.egrad = GetGrad(self.geom)
+        self.egrad = GetGrad(self.geom, self.beta)
         self.L = 2*np.pi/self.geom.symmetry_numbers
         self.center_mod = lambda x: \
                 ((x.transpose() % self.L) \
@@ -68,8 +75,7 @@ class Energy(tt.Op):
         theta, = inputs  # this will contain my variables
         theta = self.center_mod(theta)
         result = self.get_e_elect(theta, which="energy")
-        #print('x', inputs, 'energy', result)
-        outputs[0][0] = np.array(result) # output the log-likelihood
+        outputs[0][0] = np.array(self.beta*result) # output the log-likelihood
 
     def grad(self, inputs, g):
         theta, = inputs  # our parameter
@@ -80,13 +86,13 @@ class Energy(tt.Op):
 class GetGrad(tt.Op):
     itypes = [tt.dvector]
     otypes = [tt.dvector]
-    def __init__(self, geom):
+    def __init__(self, geom, beta):
         self.geom = geom
+        self.beta = beta
         self.get_grad = self.geom.get_energy_grad_at
 
     def perform(self, node, inputs, outputs):
         theta, = inputs
         #grad = self.get_grad(theta, self.ape_obj, n=self.n)
         grad = self.get_grad(theta, which="grad")
-        #print("PosteriorGrad:",theta,grad)
-        outputs[0][0] = grad
+        outputs[0][0] = self.beta*grad